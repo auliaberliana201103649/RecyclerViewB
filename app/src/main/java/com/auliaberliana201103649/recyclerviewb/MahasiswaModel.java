d0ff70be56960744049d7ccbc537c663cce900a6
201103649 Aulia Berliana <auliaaberliana@gmail.com>
2023-06-07 14:45:55 +0700

    viewb

---
+++ app/src/main/java/com/auliaberliana201103649/recyclerviewb/MahasiswaModel.java
@@ -0,0 +1,42 @@
+package com.auliaberliana201103649.recyclerviewb;
+
+public class MahasiswaModel
+{
+    private String Nim, Nama, JenisKelamin, JP;
+
+    public MahasiswaModel() {
+
+    }
+
+    public String getNim() {
+        return Nim;
+    }
+
+    public void setNim(String nim) {
+        Nim = nim;
+    }
+
+    public String getNama() {
+        return Nama;
+    }
+
+    public void setNama(String nama) {
+        Nama = nama;
+    }
+
+    public String getJenisKelamin() {
+        return JenisKelamin;
+    }
+
+    public void setJenisKelamin(String jenisKelamin) {
+        JenisKelamin = jenisKelamin;
+    }
+
+    public String getJP() {
+        return JP;
+    }
+
+    public void setJP(String JP) {
+        this.JP = JP;
+    }
+}
